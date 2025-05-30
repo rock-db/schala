9d6c1f0cb7039e60ba3a77a502eb83e182af6766
rock-db <rock-db@outlook.jp>
2025-05-30 14:10:08 +0900

    プロジェクト ファイルを追加します。

---
+++ schala/Init.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace schala
+{
+    internal class Init
+    {
+        public static void InitializeProject(bool verbose)
+        {
+            string[] dirs = new string[]
+            {
+                ".schala",
+                // refs directory structure
+                @".schala\refs",
+                @".schala\refs\heads",
+                // objects directory structure
+                @".schala\objects",
+                @".schala\objects\blobs",
+                @".schala\objects\commits",
+                @".schala\objects\trees",
+            };
+
+            foreach (string dir in dirs)
+            {
+                try
+                {
+                    Directory.CreateDirectory(dir);
+                    if (verbose)
+                    {
+                        Console.WriteLine($"Created directory: {dir}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error creating directory {dir}: {ex.Message}");
+                    return;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Project initialized successfully!");
+            Console.ResetColor();
+
+        }
+
+    }
+}
---
+++ schala/Program.cs
@@ -0,0 +1,60 @@
+﻿using schala;
+using CommandLine;
+using System;
+using CommandLine.Text;
+
+
+
+class Options
+{
+    [Option('v', "verbose", Required = false, HelpText = "Enable verbose output.")]
+    public bool Verbose { get; set; }
+
+    [Option('I', "init", Required = false, HelpText = "Initialize a Schala.")]
+    public bool Init { get; set; }
+
+}
+
+namespace Schala
+{
+    class Program
+    {
+        static string SchalaVersion = "0.1.0-alpha";
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Schala v" + SchalaVersion);
+            Console.ResetColor();
+
+            var parser = new Parser(with => with.HelpWriter = null);
+
+            var result = parser.ParseArguments<Options>(args);
+
+            result
+                .WithParsed(options =>
+                {
+                    if (options.Init)
+                    {
+                        Init.InitializeProject(options.Verbose);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No valid command provided. Use --help for more information.");
+                    }
+                })
+                .WithNotParsed(errors =>
+                {
+                    var helpText = HelpText.AutoBuild<Options>(result, h =>
+                    {
+                        h.Heading = "";
+                        h.Copyright = "Copyright (c) 2025 rock-db";
+                        return HelpText.DefaultParsingErrorsHandler(result, h);
+                    }, e=>e);
+
+                    Console.WriteLine(helpText);
+                });
+        }
+    }
+}
+
+
